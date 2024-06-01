1f15b34cd119dbc4477817bd99466d86f097ba9e
Acid Wizard <adwiz@suroviy.ru>
2024-06-01 12:01:10 +0300

    Adjusted something in server

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 logger.info('you should see these in Kibana')
 logger.info('log entry for testing for rebase')
-logger.info('Add some changes remote')
+logger.info('Add some changes remote and locally')
