6a5384140d9c74637f6dcb1fd1d1397f818f4bcd
Acid Wizard <adwiz@suroviy.ru>
2024-06-11 21:21:43 +0300

    Add all stuff

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,13 +2,12 @@ provider "aws" {
   region = "eu-west-3"
 }
 
-variable "vpc_cidr_block" {}
-variable "subnet_block" {}
-variable "avail_zone" {}
-variable "env_prefix" {}
-variable "my_ip" {
-
-}
+variable vpc_cidr_block {}
+variable subnet_block {}
+variable avail_zone {}
+variable env_prefix {}
+variable my_ip {}
+variable instance_type {}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -50,36 +49,66 @@ resource "aws_route_table_association" "a-rtb-subnet" {
   route_table_id = aws_route_table.myapp-route-table.id
 }
 
-resource "aws_security_group" "myapp-sg" {
+resource "aws_default_security_group" "default-sg" {
   name   = "myapp-sg"
   vpc_id = aws_vpc.myapp-vpc.id
 
-  ingress = [
-    {
-      from_port  = 22
-      to_port    = 22
-      protocol   = "tcp"
-      cidr_block = [var.my_ip]
-    },
+  ingress {
+    from_port  = 22
+    to_port    = 22
+    protocol   = "tcp"
+    cidr_blocks = [var.my_ip]
+    }
 
-    {
+  ingress {
       from_port  = 8080
       to_port    = 8080
       protocol   = "tcp"
-      cidr_block = ["0.0.0.0/0"]
-    },
-  ]
-
-  egress = [
-    {
-      from_port  = 0
-      to_port    = 0
-      protocol   = "-1"
-      cidr_block = ["0.0.0.0/0"]
+      cidr_blocks = ["0.0.0.0/0"]
     }
-  ]
+  
+  egress {
+    from_port  = 0
+    to_port    = 0
+    protocol   = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
 
   tags = {
-    Name : "${var.var.env_prefix}-sg"
+    Name : "${var.env_prefix}-sg"
+  }
+}
+
+data "aws_ami" "latest-amazon-linux-image" {
+    most_recent = true
+    owners = ["amazon"]
+    filter {
+        name = "name"
+        values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    }
+    filter {
+      name = "virtualization-type"
+      values = ["hvm"]
+    }
+
+}
+
+output "aws_ami_id" {
+  value = data.aws_ami.latest-amazon-linux-image.id
+}
+
+resource "aws_instance" "myapp-server" {
+    ami = data.aws_ami.latest-amazon-linux-image.id
+    instance_type = var.instance_type
+
+    subnet_id = aws_subnet.myapp-subnet-1
+    vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+    availability_zone = var.avail_zone
+
+    associate_public_ip_address = true
+    key_name = "server-key-pair "
+
+    tags = {
+        Name : "${var.env_prefix}-server"
   }
 }
