409c23eaff300944cd5e4b08bbf3c4e8833b7b01
Acid Wizard <adwiz@suroviy.ru>
2024-06-11 09:55:18 +0300

    Add network sg

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,9 @@ variable "vpc_cidr_block" {}
 variable "subnet_block" {}
 variable "avail_zone" {}
 variable "env_prefix" {}
+variable "my_ip" {
+
+}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -45,4 +48,34 @@ resource "aws_route_table" "myapp-route-table" {
 resource "aws_route_table_association" "a-rtb-subnet" {
   subnet_id      = aws_subnet.myapp-subnet-1.id
   route_table_id = aws_route_table.myapp-route-table.id
+}
+
+resource "aws_security_group" "myapp-sg" {
+  name   = "myapp-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress = [
+    {
+      from_port  = 22
+      to_port    = 22
+      protocol   = "tcp"
+      cidr_block = [var.my_ip]
+    },
+
+    {
+      from_port  = 8080
+      to_port    = 8080
+      protocol   = "tcp"
+      cidr_block = ["0.0.0.0/0"]
+    }
+  ]
+
+  egress = [
+    {
+      from_port  = 8080
+      to_port    = 8080
+      protocol   = "tcp"
+      cidr_block = ["0.0.0.0/0"]
+    }
+  ]
 }
