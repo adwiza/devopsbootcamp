9df291ba95e8655c9851dc2fda2c672e5c19c0a0
Acid Wizard <adwiz@suroviy.ru>
2024-06-11 09:36:06 +0300

    Add network subsystem

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,55 +1,48 @@
 provider "aws" {
-  region     = "eu-west-3"
+  region = "eu-west-3"
 }
 
-variable "cidr_blocks" {
-  description = "cidr blocks and names tags for vpc and subnets"
-  type = list(object({
-    cidr_block = string
-    name       = string
-  }))
+variable "vpc_cidr_block" {}
+variable "subnet_block" {}
+variable "avail_zone" {}
+variable "env_prefix" {}
 
-}
-
-variable "environment" {
-  description = "deployment environment"
-
-}
-resource "aws_vpc" "development-vpc" {
-  cidr_block = var.cidr_blocks[0].cidr_block
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr_block
   tags = {
-    Name : var.cidr_blocks[0].name
-    vpc_env : "dev"
+    Name : "${var.env_prefix}-vpc"
   }
 }
 
-resource "aws_subnet" "dev-subnet-1" {
-  vpc_id            = aws_vpc.developer-vpc.id
-  cidr_block        = var.cidr_blocks[1].cidr_block
-  availability_zone = "eu-west-3a"
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id            = aws_vpc.myapp-vpc.id
+  cidr_block        = var.subnet_block
+  availability_zone = var.avail_zone
   tags = {
-    Name : var.cidr_blocks[1].name
+    Name : "${var.env_prefix}-subnet-1"
   }
 }
 
-data "aws_vpc" "existing_vpc" {
-  default = true
-}
-
-resource "aws_subnet" "dev-subnet-2" {
-  vpc_id            = data.aws_vpc.existing_vpc.id
-  cidr_block        = "172.31.48.0/20"
-  availability_zone = "eu-west-3a"
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
   tags = {
-    Name : "subnet-1-default"
+    Name : "${var.env_prefix}-igw"
   }
 }
 
-output "vpc-id" {
-  value = aws_vpc.development-vpc.id
+resource "aws_route_table" "myapp-route-table" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+  tags = {
+    Name : "${var.env_prefix}-rtb"
+  }
 }
 
-output "dev-subnet-id" {
-  value = aws_subnet.dev-subnet-1.id
-
+resource "aws_route_table_association" "a-rtb-subnet" {
+  subnet_id      = aws_subnet.myapp-subnet-1.id
+  route_table_id = aws_route_table.myapp-route-table.id
 }
