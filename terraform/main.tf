b1f36129a04059b8795197c374ceb94b14f01807
Acid Wizard <adwiz@suroviy.ru>
2024-07-18 09:26:53 +0300

    Change az and subnet name

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "eu-west-3"
+  region = "eu-central-1"
 }
 
 variable vpc_cidr_block {}
-variable subnet_block {}
+variable subnet_cidr_block {}
 variable avail_zone {}
 variable env_prefix {}
 variable my_ip {}
@@ -20,7 +20,7 @@ resource "aws_vpc" "myapp-vpc" {
 
 resource "aws_subnet" "myapp-subnet-1" {
   vpc_id            = aws_vpc.myapp-vpc.id
-  cidr_block        = var.subnet_block
+  cidr_block        = var.subnet_cidr_block
   availability_zone = var.avail_zone
   tags = {
     Name : "${var.env_prefix}-subnet-1"
