7f5a2e914379afc9762ad2aac103e1fad8253002
Acid Wizard <adwiz@suroviy.ru>
2024-08-29 08:26:00 +0300

    feat(read status): Read instance status by boto3 lib

--- boto_example.py
+++ boto_example.py
@@ -1,35 +1,49 @@
 import boto3
 
-ec2_client = boto3.client('ec2', region_name='eu-central-1')
-ec2_resource = boto3.resource('ec2', region_name='eu-central-1')
+ec2_client = boto3.client('ec2', region_name='eu-west-3')
+ec2_resource = boto3.resource('ec2', region_name='eu-west-3')
 
-new_vpc = ec2_resource.create_vpc(
-    CidrBlock='10.0.0.0/16'
-)
+# new_vpc = ec2_resource.create_vpc(
+#     CidrBlock='10.0.0.0/16'
+# )
+#
+# new_vpc.create_subnet(
+#     CidrBlock='10.0.1.0/24',
+# )
+#
+# new_vpc.create_subnet(
+#     CidrBlock='10.0.2.0/24',
+# )
+#
+# new_vpc.create_tags(
+#     Tags=[
+#         {
+#             'Key': 'Name',
+#             'Value': 'my-vpc'
+#         },
+#     ]
+# )
+#
+# all_available_vpcs = ec2_client.describe_vpcs()
+#
+# vpcs = all_available_vpcs['Vpcs']
+#
+# for vpc in vpcs:
+#     print(vpc['VpcId'])
+#     cidr_block_assoc_sets = vpc['CidrBlockAssociationSet']
+#     for assoc_set in cidr_block_assoc_sets:
+#         print(assoc_set['CidrBlock'])
 
-new_vpc.create_subnet(
-    CidrBlock='10.0.1.0/24',
-)
+reservations = ec2_client.describe_instances()
+for reservation in reservations['Reservations']:
+    instances = reservation['Instances']
+    for instance in instances:
+        print(instance['InstanceId'], instance['State']['Name'])
 
-new_vpc.create_subnet(
-    CidrBlock='10.0.2.0/24',
-)
+statuses = ec2_client.describe_instance_status()
 
-new_vpc.create_tags(
-    Tags=[
-        {
-            'Key': 'Name',
-            'Value': 'my-vpc'
-        },
-    ]
-)
-
-all_available_vpcs = ec2_client.describe_vpcs()
-
-vpcs = all_available_vpcs['Vpcs']
-
-for vpc in vpcs:
-    print(vpc['VpcId'])
-    cidr_block_assoc_sets = vpc['CidrBlockAssociationSet']
-    for assoc_set in cidr_block_assoc_sets:
-        print(assoc_set['CidrBlock'])
+for status in statuses['InstanceStatuses']:
+    ins_status = status['InstanceStatus']['Status']
+    sys_status = status['SystemStatus']['Status']
+    state = status['InstanceState']['Name']
+    print(f'Instance {status["InstanceId"]} is {state} with instance status is {ins_status} and system_status is {sys_status}')
